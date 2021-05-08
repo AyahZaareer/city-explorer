2fe392119a8b64577d37f7a7999d09fa827067f1
AyahZaareer <ayah@LTUCSTUDENT-L075.localdomain>
2021-05-08 04:37:54 +0300

    class8

--- src/com/movie.js
+++ src/com/movie.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css';
 
 import Card from 'react-bootstrap/Card';
+import { CardGroup } from 'react-bootstrap';
 
 
 export class Movie extends Component {
@@ -11,20 +12,19 @@ export class Movie extends Component {
         console.log(data);
         return (
           <div>
-            {/* <p>{data.title}</p>
-            <img src={`https://www.themoviedb.org/t/p/original/${data.img}`} alt='' />
-            <p>{data.description}</p> */}
-            <Card border="primary" style={{ width: '80rem', height: '15rem', marginTop: '30px' }}>
-              <Card.Header>{data.title}</Card.Header>
-              <Card.Body>
-                <Card.Img variant="top" src={`https://www.themoviedb.org/t/p/original/${data.img}`} alt='null' />
 
-                <Card.Text>
-                  {data.description}
-                </Card.Text>
-              </Card.Body>
-            </Card>
-            <br />
+            <CardGroup>
+              <Card style={{ margin: '20px', width: '100rem' }}>
+                <Card.Img style={{ width: '100px' }} variant="top" src={`https://www.themoviedb.org/t/p/original/${data.img}`} alt='null' />
+                <Card.Body>
+                  <Card.Title>{data.title}</Card.Title>
+                  <Card.Text>
+                    {data.description}
+                  </Card.Text>
+                </Card.Body>
+
+              </Card>
+            </CardGroup>
 
           </div>
         );
